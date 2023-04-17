1175339c644041db151042db78fb5c51aed52c43
Dimi <fe.dimitrisk@gmail.com>
2023-04-17 02:04:05 +0000

    testing

---
+++ testme2/src/pages/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoList from './TodoList'
+
+function App() {
+  <>
+    <TodoList />
+    <input type = "text" />
+  </>
+}
+
+
+export default App;
---
+++ testme2/src/pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import React from 'react'
+import TodoList from './TodoList'
+
+function App() {
+  <>
+    <TodoList />
+    <input type = "text" />
+  </>
+}
+
+
+export default App;
